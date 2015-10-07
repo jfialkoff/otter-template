3b0d70b1f8d294fbd78b58215df0eb87252332f0
Joshua Fialkoff <joshua.fialkoff@setaris.com>
2015-10-06 20:31:49 -0400

    Finished whitenoise setup.

--- ot_myprojectdir/settings.py
+++ ot_myprojectdir/settings.py
@@ -132,7 +132,6 @@ STATICFILES_FINDERS = (
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'pipeline.storage.PipelineCachedStorage'
 
 ##################### Compressor #####################
 COMPRESS_PRECOMPILERS = (
